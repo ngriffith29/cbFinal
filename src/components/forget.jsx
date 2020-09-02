da07782695b78f64521eeaa91e20bf40cd33ea7d
ngriffith29 <ngriffith29@student.egcc.edu>
2020-09-02 09:42:03 -0400

    finishing touches

--- src/components/forget.jsx
+++ src/components/forget.jsx
@@ -11,7 +11,7 @@ function Ret(props) {
     const [email, setemail] = useState("")
 
 
-    const [grade, setGrade] = useState(["Pick A Grade",6, 7, 8])
+    const [grade, setGrade] = useState(["Pick A Grade",0,1,2,3,4,5,6, 7, 8,9,10,11,12])
     const [pickedGrade, setPickedGrade] = useState(grade[0])
 
     console.log(grade)
